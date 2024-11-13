0aa6166566a18a44e626c95e0d2cf873afd89539
Kmliu1234 <00001088301757SP@al.educacao.sp.gov.br>
2024-11-13 11:58:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Exploração Espacial',
+    'Qual foi a primeira missão tripulada à Lua?',
+    'A primeira missão tripulada à Lua foi a Apollo 11, em 1969, com os astronautas Neil Armstrong e Buzz Aldrin'
+)
+
+criaCartao(
+    'Exploração Espacial',
+    'O que é a Estação Espacial Internacional (EEI)?',
+    'A Estação Espacial Internacional (EEI) é um laboratório espacial modular onde astronautas de diferentes países conduzem pesquisas científicas no espaço'
+)
+
+criaCartao(
+    'Exploração Espacial',
+    'Quem foi o primeiro ser humano a viajar para o espaço?',
+    'O primeiro ser humano a viajar para o espaço foi Yuri Gagarin, um cosmonauta soviético, em 1961'
+)
+
+criaCartao(
+    'Exploração Espacial',
+    'Qual planeta foi visitado pela missão Voyager 1?',
+    'A missão Voyager 1 visitou Júpiter e Saturno, e atualmente está viajando para o espaço interestelar'
+)
